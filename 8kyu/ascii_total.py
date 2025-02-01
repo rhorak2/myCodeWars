d5366ee5c2a95279a1321180dc93ffb8841166f9
rhorak2 <155626687+rhorak2@users.noreply.github.com>
2025-02-01 18:40:21 +0100

    Add ascii_total.py application

---
+++ 8kyu/ascii_total.py
@@ -0,0 +1,21 @@
+# You'll be given a string, and have to return the sum of all characters as an int. The function should be able to handle all printable ASCII characters.
+#
+# Examples:
+#
+# uniTotal("a") == 97
+# uniTotal("aaa") == 291
+
+def uni_total(s):
+    total: int = 0
+    for i in s:
+        total += ord(i)
+    return(total)
+
+# best practices
+# def uni_total(string):
+#     return sum(map(ord, string))
+# or
+# def uni_total(s):
+#     return sum(ord(c) for c in s)
+
+
