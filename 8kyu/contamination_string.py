e5ba144719da8d4fe73e16448604c669d2966f0c
rhorak2 <155626687+rhorak2@users.noreply.github.com>
2025-02-02 10:07:59 +0100

    add contamination_string.py application

---
+++ 8kyu/contamination_string.py
@@ -0,0 +1,25 @@
+# An AI has infected a text with a character!!
+#
+# This text is now fully mutated to this character.
+#
+# Starting with the original text, and given a character, return the text once it has been mutated so that all of the characters in the original text have been replaced with the character.
+#
+# If the text or the character are empty, return an empty string.
+# There will never be a case when both are empty as nothing is going on!!
+#
+# Note: The character is a string of length 1 or an empty string.
+#
+# Example
+# text before = "abc"
+# character   = "z"
+# text after  = "zzz"
+
+def contamination(text, char):
+    mutated_str: str = ""
+    if len(text) and len(char) > 0:
+        mutated_str = len(text) * char
+    return mutated_str
+
+# best practices
+# def contamination(text, char):
+#   return char*len(text)
